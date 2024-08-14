9c07bbb29e7980add31669836383e4f6f60998f5
Matt <mt.collins404@gmail.com>
2024-08-15 02:44:15 +1000

    Added pfp + Redid design + More text

--- js/add_navbar.js
+++ js/add_navbar.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('../html/navbar.html')
+    fetch('./html/navbar.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('navbar').innerHTML = data;
